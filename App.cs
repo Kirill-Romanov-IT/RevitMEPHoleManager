a96899f83f203ba48e71521f0a1c9ff88af16276
Your <k.romanov@corp.sg.local>
2025-08-04 12:13:28 +0300

    Add project files.

---
+++ App.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.UI;
+using System.Reflection;
+
+namespace RevitMEPHoleManager
+{
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            const string tabName = "MEP Hole Manager";
+            try { application.CreateRibbonTab(tabName); } catch { }
+
+            RibbonPanel panel = application.CreateRibbonPanel(tabName, "Основное");
+
+            string asmPath = Assembly.GetExecutingAssembly().Location;
+
+            PushButtonData btn = new PushButtonData(
+                "HoleManagerBtn",
+                "Запуск",
+                asmPath,
+                "RevitMEPHoleManager.ShowGuiCommand");
+
+            panel.AddItem(btn);
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ ShowGuiCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.Windows;
+
+namespace RevitMEPHoleManager
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class ShowGuiCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            MainWindow window = new MainWindow();
+            window.ShowDialog();
+            return Result.Succeeded;
+        }
+    
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
